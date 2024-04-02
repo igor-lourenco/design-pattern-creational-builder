b957a5d3d2b54c2582aa2409e0fc8a8d9087ea3c
igor-lourenco <igor.lourencosantos@gmail.com>
2024-04-02 10:31:13 -0300

    Usando a implementacao antes de usar o padrao de projeto - Builder.

--- src/main/java/creational/builder/Client.java
+++ src/main/java/creational/builder/Client.java
@@ -1,12 +1,35 @@
 package creational.builder;
 
+import creational.builder.meal.before.FastFoodMeal;
+
 public class Client {
 
+    /*
+     * Problemas:
+     * 
+     * - Criação de objetos complexos: O Builder é útil quando você precisa criar
+     * objetos que requerem muitos parâmetros ou quando deseja que o processo de
+     * criação de objetos seja mais flexível12.
+     * 
+     * - Evitar a passagem de parâmetros desnecessários: Para evitar a passagem de
+     * vários parâmetros que não serão usados, o padrão Builder sugere que você
+     * extraia o código de construção do objeto para fora de sua própria classe e
+     * mova ele para objetos separados chamados builders3.
+     * 
+     * - Reutilização, flexibilidade e fácil manutenção: O Builder é focado em criar
+     * objetos complexos de maneira reutilizável, flexível e de fácil manutenção
+     */
     public static void main(String[] args) {
-	
-	
-	System.out.println("Projeto inicial.");
-	
+
+	FastFoodMeal burguerCombo = new FastFoodMeal("Coke", "CheeseBurguer", "Fries");
+	System.out.println(burguerCombo);
+
+	FastFoodMeal justFries = new FastFoodMeal(null, null, "Fries");
+	System.out.println(justFries);
+
+	FastFoodMeal heartAtackCombo = new FastFoodMeal("Milk Shake", "Monster Burguer", "Large Fries", "Fudge Cake",
+		"2 Kilograms");
+	System.out.println(heartAtackCombo);
 
     }
 }
