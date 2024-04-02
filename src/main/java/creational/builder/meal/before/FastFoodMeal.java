63bd3a550ff3711fb6e2b3bfd04d1604512ae9f9
igor-lourenco <igor.lourencosantos@gmail.com>
2024-04-02 10:18:45 -0300

    Criacao da classe FastFoodMeal antes da implemetacao do padrao de projeto - Builder.

---
+++ src/main/java/creational/builder/meal/before/FastFoodMeal.java
@@ -0,0 +1,89 @@
+package creational.builder.meal.before;
+
+public class FastFoodMeal {
+
+    private String drink;
+    private String main;
+    private String side;
+    private String dessert;
+    private String gift;
+
+    // Muitos construtores
+    // É difícil saber os parâmetros do pedido
+
+    public FastFoodMeal(String drink) {
+	this.drink = drink;
+    }
+
+    public FastFoodMeal(String drink, String main) {
+	this.drink = drink;
+	this.main = main;
+    }
+
+    public FastFoodMeal(String drink, String main, String side) {
+	this.drink = drink;
+	this.main = main;
+	this.side = side;
+    }
+
+    public FastFoodMeal(String drink, String main, String side, String dessert) {
+	this.drink = drink;
+	this.main = main;
+	this.side = side;
+	this.dessert = dessert;
+    }
+
+    public FastFoodMeal(String drink, String main, String side, String dessert, String gift) {
+	this.drink = drink;
+	this.main = main;
+	this.side = side;
+	this.dessert = dessert;
+	this.gift = gift;
+    }
+
+    public String getDrink() {
+	return drink;
+    }
+
+    public void setDrink(String drink) {
+	this.drink = drink;
+    }
+
+    public String getMain() {
+	return main;
+    }
+
+    public void setMain(String main) {
+	this.main = main;
+    }
+
+    public String getSide() {
+	return side;
+    }
+
+    public void setSide(String side) {
+	this.side = side;
+    }
+
+    public String getDessert() {
+	return dessert;
+    }
+
+    public void setDessert(String dessert) {
+	this.dessert = dessert;
+    }
+
+    public String getGift() {
+	return gift;
+    }
+
+    public void setGift(String gift) {
+	this.gift = gift;
+    }
+
+    @Override
+    public String toString() {
+	return "FastFoodMeal [drink=" + drink + ", main=" + main + ", side=" + side + ", dessert=" + dessert + ", gift="
+		+ gift + "]";
+    }
+}
