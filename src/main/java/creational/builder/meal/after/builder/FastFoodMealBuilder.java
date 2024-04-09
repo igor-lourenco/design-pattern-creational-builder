d54d06219c7770c4aab4e632497bc326d89c58ff
igor-lourenco <igor.lourencosantos@gmail.com>
2024-04-09 15:19:48 -0300

    Implementacao da classe abstrata FastFoodMealBuilder.

---
+++ src/main/java/creational/builder/meal/after/builder/FastFoodMealBuilder.java
@@ -0,0 +1,25 @@
+package creational.builder.meal.after.builder;
+
+import creational.builder.meal.after.model.FastFoodMeal;
+
+public abstract class FastFoodMealBuilder {
+    protected FastFoodMeal meal;
+
+    public FastFoodMealBuilder() {
+	meal = new FastFoodMeal();
+    }
+
+    public FastFoodMeal getMeal() {
+	return meal;
+    }
+
+    public abstract void buildDrink();
+
+    public abstract void buildMain();
+
+    public abstract void buildSide();
+
+    public abstract void buildDessert();
+
+    public abstract void buildGift();
+}
