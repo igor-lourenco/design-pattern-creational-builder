16b1ed9cab5fbe8c86a1d1ca35b7f7839b58d14d
igor-lourenco <igor.lourencosantos@gmail.com>
2024-04-16 09:59:56 -0300

    Implementacao da classe Client para o fluent.

---
+++ src/main/java/creational/builder/meal/fluent/Client.java
@@ -0,0 +1,30 @@
+package creational.builder.meal.fluent;
+
+import creational.builder.meal.fluent.model.FastFoodMeal;
+import creational.builder.meal.fluent.model.builder.FastFoodMealBuilder;
+
+public class Client {
+
+    public static void main(String[] args) {
+	FastFoodMeal burguerCombo = new FastFoodMealBuilder("Fries")
+		.andMain("CheeseBurger")
+		.forDrink("Code")
+		.thatsAll();
+	
+	System.out.println(burguerCombo);
+
+	FastFoodMeal justFries = new FastFoodMealBuilder("Fries")
+		.thatsAll();
+	
+	System.out.println(justFries);
+
+	FastFoodMeal heartAtackCombo = new FastFoodMealBuilder("Large Fries")
+		.andMain("Monster Burguer")
+		.forDrink("Milk Shake")
+		.andDessert("Fudge Cake")
+		.andGift("2 Kilograms")
+		.thatsAll();
+	
+	System.out.println(heartAtackCombo);
+    }
+}
