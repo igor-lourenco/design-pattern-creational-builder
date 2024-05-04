0ba342a3f945d12ef38c4d5b8d5e0248f1b7923e
igor-lourenco <igor.lourencosantos@gmail.com>
2024-05-04 13:26:46 -0300

    Implementacao da classe Client.

---
+++ src/main/java/creational/builder/spoleto/functional/Client.java
@@ -0,0 +1,28 @@
+package creational.builder.spoleto.functional;
+
+import creational.builder.spoleto.functional.model.Pasta;
+import creational.builder.spoleto.functional.model.Size;
+
+public class Client {
+
+    public static void main(String[] args) {
+	Pasta p1 = new Pasta.Builder(Size.LARGE)
+		.now();
+	System.out.println(p1);
+
+	Pasta p2 = new Pasta.Builder(Size.SMALL)
+		.withToppings("Bacon", "Broccoli")
+		.withSauces("Tomato")
+		.withCheese()
+		.now();
+	System.out.println(p2);
+
+	Pasta p3 = new Pasta.Builder(Size.STANDARD)
+		.withToppings("Garlic")
+		.withSauces("Funghi", "Cheese")
+		.withCheese()
+		.withPepper()
+		.now();
+	System.out.println(p3);
+    }
+}
